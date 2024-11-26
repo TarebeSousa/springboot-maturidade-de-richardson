66ae5c9264ae65a8a86793a0d9e7df571263e212
Tarebe Sousa <tarebesousadev@gmail.com>
2024-11-26 14:04:53 -0300

    Feito

---
+++ src/main/java/com/tarebe/springbootmaturidade/SpringbootmaturidadeApplication.java
@@ -0,0 +1,13 @@
+package com.tarebe.springbootmaturidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmaturidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmaturidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarebe/springbootmaturidade/SpringbootmaturidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarebe.springbootmaturidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmaturidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
